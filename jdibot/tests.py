d5e2063fab4d3cc077f5fc45c71283a874145615
Egor Luginin <luginine@mail.ru>
2016-07-11 23:41:33 +0300

    added some tests

--- jdibot/tests.py
+++ jdibot/tests.py
@@ -12,7 +12,36 @@ def SQLInsertTest1():
 
 	return "Complete"
 
+def SQLSelectTest1():
+	db_worker = SQLighter(config.database_name)
+
+	fire_time = 1000
+
+	list = db_worker.select_fire_list(fire_time)
+
+	db_worker.close()
+
+	print(list)
+	return "Complete"
+
+def SQLUpdateTest1():
+	db_worker = SQLighter(config.database_name)
+
+	fire_time = 1000
+
+	list = db_worker.select_fire_list(fire_time)
+	for obj in list:
+		print(obj.period)
+		obj.firetime += obj.period
+	db_worker.update_fire_list(list)
+	list = db_worker.select_fire_list(fire_time+100)
+	db_worker.close()
+
+	print(list)
+	return "Complete"
 
 if __name__ == '__main__':
 
 	print(SQLInsertTest1())
+	print(SQLSelectTest1())
+	print(SQLUpdateTest1())
